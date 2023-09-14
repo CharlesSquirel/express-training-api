8e703d90c9b9a5e725934d510ea28bd83566a510
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-09-14 13:04:50 +0200

    Initial commit get

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from Nerdbord!')
-})
+app.get('/trains', (req, res) => {
+    fs.readFile('data/trains.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Wystąpił błąd podczas odczytu pliku.')
+        }
+        else {
+            try {
+
+                const trainsData = JSON.parse(data);
+                const formattedData = JSON.stringify(trainsData, null, 2);
+                res.setHeader('Content-Type', 'application/json');
+                res.send(formattedData);
+            }
+            catch(err) {
+                console.error(error);
+                res.status(500).send('Wystąpił błąd podczas parsowania danych JSON.');
+            }
+        }
+    })
+});
+
 
 app.listen(PORT, () => {
-    console.log('Server listening on port 3000')
-})
+    console.log(`Serwer nasłuchuje na porcie ${PORT}`);
+});
