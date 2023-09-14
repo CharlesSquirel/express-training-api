0be41bc45a3bb67aba70e517e6365052c2f38d94
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-09-14 13:16:04 +0200

    Commit post API

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/trains', (req, res) => {
     fs.readFile('data/trains.json', 'utf8', (err, data) => {
         if (err) {
@@ -24,6 +26,35 @@ app.get('/trains', (req, res) => {
             }
         }
     })
+});
+
+app.post('/trains', (req, res) => {
+    const newTrain = req.body;
+
+    fs.readFile('data/trains.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Wystąpił błąd podczas odczytu pliku.');
+        } else {
+            try {
+                const trainsData = JSON.parse(data);
+
+                trainsData.push(newTrain);
+
+                fs.writeFile('data/trains.json', JSON.stringify(trainsData, null, 2), (err) => {
+                    if (err) {
+                        console.error(err);
+                        res.status(500).send('Wystąpił błąd podczas zapisywania danych.');
+                    } else {
+                        res.status(201).json(newTrain);
+                    }
+                });
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Wystąpił błąd podczas parsowania danych JSON.');
+            }
+        }
+    });
 });
 
 
